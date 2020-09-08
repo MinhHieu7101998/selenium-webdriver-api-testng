7daa7fa055847eb331598d83c4149890712628be
Vo Minh Hieu <minhhieu7101998@gamil.com>
2020-09-08 23:05:57 +0700

    Topic 09 - Exercise Xpath/CSS (5-6)

--- selenium/webdriver/Topic_03_Locator_Xpath_Exercise.java
+++ selenium/webdriver/Topic_03_Locator_Xpath_Exercise.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -20,7 +21,7 @@ public class Topic_03_Locator_Xpath_Exercise {
 		driver.manage().window().maximize();
 	}
 
-	@Test
+	@Test(enabled = false)
 	public void TC_01_Login_With_Empty_Email_And_Password() {
 		driver.get("http://live.demoguru99.com/");
 		driver.findElement(By.xpath(".//div[@class='footer']//a[@title='My Account']")).click();
@@ -36,7 +37,7 @@ public class Topic_03_Locator_Xpath_Exercise {
 		Assert.assertEquals(passwordMessageError, "This is a required field.");
 	}
 
-	@Test
+	@Test(enabled = false)
 	public void TC_02_Login_With_Invalid_Email() {
 		driver.get("http://live.demoguru99.com/");
 		driver.findElement(By.xpath(".//div[@class='footer']//a[@title='My Account']")).click();
@@ -50,7 +51,7 @@ public class Topic_03_Locator_Xpath_Exercise {
 
 	}
 
-	@Test
+	@Test(enabled = false)
 	public void TC_03_Login_With_Invalid_Password() {
 		driver.get("http://live.demoguru99.com/");
 		driver.findElement(By.xpath(".//div[@class='footer']//a[@title='My Account']")).click();
@@ -65,8 +66,8 @@ public class Topic_03_Locator_Xpath_Exercise {
 				"Please enter 6 or more characters without leading or trailing spaces.");
 	}
 
-	@Test
-	public void TC_03_Login_With_Incorrect_Password() {
+	@Test(enabled = false)
+	public void TC_04_Login_With_Incorrect_Password() {
 		driver.get("http://live.demoguru99.com/");
 		driver.findElement(By.xpath(".//div[@class='footer']//a[@title='My Account']")).click();
 		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("automation@gmail.com");
@@ -76,9 +77,46 @@ public class Topic_03_Locator_Xpath_Exercise {
 
 		String messageError = driver.findElement(By.xpath(".//li[@class='error-msg']//span")).getText();
 		Assert.assertEquals(messageError, "Invalid login or password.");
-
+		
 	}
-
+	
+	@Test(enabled = false)
+	public void TC_05_Login_With_Valid_Email_And_Password() {
+		driver.get("http://live.demoguru99.com/");
+		driver.findElement(By.xpath(".//div[@class='footer']//a[@title='My Account']")).click();
+		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("automation_13@gmail.com");
+		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("123123");
+		driver.findElement(By.xpath(".//*[@id='send2']")).click();
+		String myDashboardText =  driver.findElement(By.xpath(".//div[@class='dashboard']//h1")).getText();
+		Assert.assertEquals(myDashboardText,"MY DASHBOARD");
+		driver.findElement(By.xpath(".//p[@class='hello']/strong[contains(text(),'Hello, Automation Testing!')]")).isDisplayed();
+		driver.findElement(By.xpath(".//div[@class='col-1']//div[@class='box-content']//p[contains(.,'Automation Testing')]")).isDisplayed();
+		driver.findElement(By.xpath(".//div[@class='col-1']//div[@class='box-content']//p[contains(.,'automation_13@gmail.com')]")).isDisplayed();
+		driver.findElement(By.xpath(".//span[@class='label' and text()='Account'] ")).click();
+		driver.findElement(By.xpath(".//a[@title=\"Log Out\"]")).click();
+	}
+	@Test
+	public void TC_06_Create_A_New_Account() {
+		String email = "PhamVy" + randomEmail() + "@gmail.com";
+		driver.get("http://live.demoguru99.com/");
+		driver.findElement(By.xpath(".//div[@class='footer']//a[@title='My Account']")).click();
+		driver.findElement(By.xpath(".//span[contains(text(),'Create an Account')]")).click();
+		driver.findElement(By.xpath(".//*[@id='firstname']")).sendKeys("Pham");
+		driver.findElement(By.xpath(".//*[@id='lastname']")).sendKeys("Vy");
+		driver.findElement(By.xpath(".//*[@id='email_address']")).sendKeys(email);
+		driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("1234567");
+		driver.findElement(By.xpath(".//*[@id='confirmation']")).sendKeys("1234567");
+		driver.findElement(By.xpath(".//*[@id='form-validate']//button[@title='Register']")).click();
+		driver.findElement(By.xpath(".//span[contains(text(),'Thank you for registering with Main Website Store.')]")).isDisplayed();
+		driver.findElement(By.xpath(".//div[@class='col-1']//div[@class='box-content']//p[contains(.,'Pham Vy')]")).isDisplayed();
+		Assert.assertEquals(driver.findElement(By.xpath(".//div[@class='col-1']//div[@class='box-content']//p")).getText().contains(email), true);
+		driver.findElement(By.xpath(".//span[@class='label' and text()='Account'] ")).click();
+		driver.findElement(By.xpath(".//a[@title=\"Log Out\"]")).click();
+	}
+	public int randomEmail() {
+		Random rand = new Random();
+		return rand.nextInt(9999999);
+	}
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
