59b1807d664b6b87673a1b58e29d5b9dc5d0058e
Vo Minh Hieu <minhhieu7101998@gamil.com>
2020-09-11 23:28:39 +0700

    Topic_12_Selenium_Ecosystem

---
+++ selenium/webdriver/Topic_04_Browser.java
@@ -0,0 +1,43 @@
+package webdriver;
+	
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_04_Browser {
+	WebDriver driver;
+	
+	@Test
+	public void TC_01_Run_On_Firefox() {
+		driver = new FirefoxDriver();
+		driver.get("https://www.youtube.com/?gl=VN");
+		driver.quit();
+	}
+
+	@Test
+	public void TC_02_Run_On_Chrome() {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vo Minh Hieu\\git\\selenium-webdriver-api-testng\\BrowserDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.youtube.com/?gl=VN");
+		driver.quit();
+	}
+	
+
+	@Test
+	public void TC_03_Run_On_Edge() {
+		
+		System.setProperty("webdriver.edge.driver", "C:\\Users\\Vo Minh Hieu\\git\\selenium-webdriver-api-testng\\BrowserDriver\\msedgedriver.exe");
+		driver = new EdgeDriver();
+		driver.get("https://www.youtube.com/?gl=VN");
+		driver.quit();
+	}
+}
+	
